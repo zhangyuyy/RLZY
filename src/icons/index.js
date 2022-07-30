ebb08af7c9ea1bcdcdf9614ac8e0bb12a289d48a
zhangyu <2863680540@qq.com>
2022-07-30 17:24:45 +0800

    项目基本初始化操作

---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+// 注册全局组件
+Vue.component('svg-icon', SvgIcon)
+// 一次性加载所有的/svg目录下的所有svg文件
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // 网站标题
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // logo显示
+  sidebarLogo: true
+}
