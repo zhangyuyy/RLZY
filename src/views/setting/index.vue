1bbfe232925dc3c1a1467811c943d37cb0199546
zhangyu <2863680540@qq.com>
2022-08-11 09:08:25 +0800

    完成公司设置基本结构

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>公司设置</h2>
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="角色管理" name="first">
+          <el-button type="primary">主要按钮</el-button>
+          <!-- 表格 -->
+          <template>
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="日期" width="180">
+              </el-table-column>
+              <el-table-column prop="name" label="姓名" width="180">
+              </el-table-column>
+              <el-table-column prop="address" label="地址"> </el-table-column>
+            </el-table>
+          </template>
+        </el-tab-pane>
+        <el-tab-pane label="公司信息" name="second">公司信息</el-tab-pane>
+      </el-tabs>
     </div>
   </div>
 </template>
@@ -9,7 +24,31 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      activeName: 'first',
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
   },
 
   created() {},
